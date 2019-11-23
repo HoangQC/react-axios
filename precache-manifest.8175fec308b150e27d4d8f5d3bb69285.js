4f06de240cdeb816c960f9a73a9706ee1dfd88c5
unknown <hoangcuong.sw@gmail.com>
2019-11-24 01:21:40 +0700

    Updates

---
+++ precache-manifest.8175fec308b150e27d4d8f5d3bb69285.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e4b046e3bb3f8f96d1437ef0873791",
+    "url": "/react-axios/index.html"
+  },
+  {
+    "revision": "3ae3a4be0093b51837aa",
+    "url": "/react-axios/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "cc69b872704197804c35",
+    "url": "/react-axios/static/js/2.52aabbf1.chunk.js"
+  },
+  {
+    "revision": "3ae3a4be0093b51837aa",
+    "url": "/react-axios/static/js/main.33c454de.chunk.js"
+  },
+  {
+    "revision": "274122c6881efe119895",
+    "url": "/react-axios/static/js/runtime-main.b41c640d.js"
+  }
+]);
